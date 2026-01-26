a5f9bea6fe5781936aaccdfafc7ef4340e2cff1f
TecBot <technic.robot.35@gmail.com>
2026-01-26 00:05:10 -0600

    Add 2026 tops and posts

--- tag_analysis/compute_top_tags.py
+++ tag_analysis/compute_top_tags.py
@@ -8,7 +8,6 @@ def proc_opts():
     parser = argparse.ArgumentParser(description="Analyze and graph artwork tags.")
     parser.add_argument("filename", help="Input file containing tag data")
     parser.add_argument("outdir", help="Output directory (default: ./)")
-    parser.add_argument('--frequency', help = 'Group by frequency in months', default="6ME", type=str)  
     parser.add_argument('--time-start', help = 'Only consider entries after this date')  
     parser.add_argument('--time-stop', help = 'Only consider entries after this date')  
     return parser.parse_args()
